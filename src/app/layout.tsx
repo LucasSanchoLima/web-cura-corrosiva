f648904e45c9b2a1ca9de3af07182970b2c57de6
LucasSancho <lucassancholima@gmail.com>
2024-07-09 15:06:49 -0300

    Correcao de erro 1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import Header from "@/components/header/Header";
 import { MenuContextProvider } from "@/contexts/menuContext";
 import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
-import Head from "next/head";
 import { UserContextProvider } from "@/contexts/userContext";
 import { PopUpContextProvider } from "@/contexts/popUpContext";
 import PopUp from "@/components/popUp/PopUp";
@@ -23,16 +22,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <Head>
-        <script
-          src="/pdfjs/pdf.mjs"
-          type="module"
-        />
-      </Head>
-      {/* <GoogleTagManager gtmId="GTM-N22LR9RD" /> */}
       <GoogleTagManager gtmId="G-3ZRQWLF62F" />
       <GoogleAnalytics gaId="G-3ZRQWLF62F" />
-      {/* <GoogleAnalytics gaId="G-3ZRQWLF62F" /> */}
       <body className={inter.className + " pt-9 text-white bg-zinc-950 bg-none"}>
         <UserContextProvider>
           <MenuContextProvider>
