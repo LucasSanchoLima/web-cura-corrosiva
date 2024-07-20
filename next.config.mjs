98adb71ce45cd1735f9f5b0956258e865a41bf53
LucasSancho <lucassancholima@gmail.com>
2024-07-20 17:59:46 -0300

    allowed origin

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { esmExternals: "loose" },
+  experimental: { esmExternals: "loose", serverActions: { allowedOrigins: ["curacorrosiva.com", "corrosivecure.com"] } },
   webpack: (config) => {
     config.module.rules.push({ test: /\.html$/, use: "raw-loader" });
     return config;
-  }
+  },
 };
 
 export default nextConfig;
