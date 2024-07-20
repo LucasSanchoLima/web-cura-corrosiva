3072cfcc70ca22fe1898f920f727b08b89b3dc7f
LucasSancho <lucassancholima@gmail.com>
2024-07-20 17:02:44 -0300

    Sem .env.local

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY_FIREBASE,
+  apiKey: "AIzaSyDWtk18Ggyb4zGHybiWl4HmU13_yMnoqw4",
   authDomain: "cura-corrosiva.firebaseapp.com",
   projectId: "cura-corrosiva",
   storageBucket: "cura-corrosiva.appspot.com",
   measurementId: "G-VJPDEZMQVC",
-  messagingSenderId: process.env.NEXT_PUBLIC_MES_SENDER_ID_FIREBASE,
-  appId: process.env.NEXT_PUBLIC_APP_ID_FIREBASE,
+  messagingSenderId: "806781857327",
+  appId: "1:806781857327:web:05e0bc16ce3324c0b2e0da",
 };
 
 // Initialize Firebase
